8bc5a79db7694059139b492760b6cd9e018c0190
g-WSL <gsakore22@gmail.com>
2025-09-04 16:46:43 +0530

    - added spec cases for one and two input numbers in string - updated method for handling non digit string and multiple inputs sperated by comma(,)

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,8 +1,11 @@
 module StringCalculator
 
   def self.add(string_numbers)
-    puts "working .. #{string_numbers}"
     return 0 if string_numbers.empty?
+    return 0 unless string_numbers.match?(/\d/)
+
+    numbers = string_numbers.split(",").map(&:to_i)
+    return numbers.sum
   end
 
 end
--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -4,11 +4,38 @@ RSpec.describe StringCalculator do
   describe "#add" do
     context "when input is empty" do
       it "returns 0" do
-        expect(StringCalculator.add("")).to eq(0)
+        expect(described_class.add("")).to eq(0)
       end
     end
 
-    context "when input has one number" do
+    context "when input is not empty" do
+      context "input does not contain any number" do
+        it "returns 0" do
+          expect(described_class.add("asdfsdf")).to eq(0)
+        end
+      end
+
+      context "when input has one number" do
+        it "returns same number as input" do
+          expect(described_class.add("1")).to eq(1)
+        end
+
+        it "returns 23 for input 23" do
+          expect(described_class.add("23")).to eq(23)
+        end
+      end
+
+      context "when input has two numbers" do
+        it "returns sum of two numbers" do
+          expect(described_class.add("1,2")).to eq(3)
+        end
+
+        it "returns sum of 23 and 54 as 77" do
+          expect(described_class.add("23,54")).to eq(77)
+        end
+      end
+      
+
 
     end
   end
